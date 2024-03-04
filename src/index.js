ea02eecd1320d924eb12a660d493986f6d1ba779
Amderson Hernandez <ahernandezdep@gmail.com>
2024-03-04 12:23:51 -0500

    "agregando nodo de imagen"

--- src/index.js
+++ src/index.js
@@ -2,5 +2,25 @@
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
+//*1.- debo crear un imagen
+//*2.- agregar #imagen
+// *3.- referencia de lo que habia hecho en HTML:
+//<div class="p-4">
+//             <img class="mx-auto"
+//               width="300"
+//               src="https://randomfox.ca/images/18.jpg"
+//               alt=""
+//             />
+//           </div>
+const createImageNode = () => {
+  const imagen = document.createElement("img");
+  imagen.class = "mx-auto";
+  imagen.width = "300";
+  imagen.src = "https://randomfox.ca/images/22.jpg";
 
-console.log('Happy hacking :)')
+  return imagen;
+};
+
+const nuevaImagen = createImageNode();
+const mountNode = document.getElementById("images");
+mountNode.appendChild(nuevaImagen);
