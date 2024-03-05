16976d48c2edafef401576eeca4d174b643e6efe
Amderson Hernandez <ahernandezdep@gmail.com>
2024-03-05 11:14:38 -0500

    "aplicando hyperscript"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import h from "hyperscript";
 import { registerImage } from "./lazy";
 import { createImageNodes } from "./utils";
 
--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,25 @@
+import h from "hyperscript";
+
 const minimum = 1;
 const maximum = 122;
 
 const random = () => Math.floor(Math.random() * (maximum - minimum)) + minimum;
 
 export const createImageNodes = () => {
-  const wrapper = document.createElement("div");
-  wrapper.className = "p-4";
+  //   const wrapper = document.createElement("div");
+  //   wrapper.className = "p-4";
+  const wrapper = h("div.p-4"); // hyperscript resume las dos lineas anteriores
 
-  const image = document.createElement("img");
-  image.className = "mx-auto rounded-md bg-gray-300";
-  image.src =
-    "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=";
-  image.dataset.src = `https://randomfox.ca/images/${random()}.jpg`;
-  image.width = 320;
+  //   const image = document.createElement("img");
+  //   image.className = "mx-auto rounded-md bg-gray-300";
+  //   image.src =
+  //     "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=";
+  //   image.dataset.src = `https://randomfox.ca/images/${random()}.jpg`;
+  //   image.width = 320;
+  const image = h("img.mx-auto.rounded-md.bg-gray-300", {
+    width: "320",
+    "data-src": `https://randomfox.ca/images/${random()}.jpg`,
+  });
 
   wrapper.append(image);
 
