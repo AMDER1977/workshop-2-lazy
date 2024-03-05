a23119e0926dac8f1d0b429ed9a929dfbb2c138a
Amderson Hernandez <ahernandezdep@gmail.com>
2024-03-05 18:08:10 -0500

    "return snow"

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,12 +1,10 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: "./build",
-    src: "./build/_dist_",
+    public: "/",
+    src: "/_dist_",
   },
   buildOptions: {
-    out: "./build", // Carpeta de salida (ajusta según tu preferencia)
-    formats: ["esm", "cjs", "umd", "iife"],
+    baseUrl: "",
   },
-  exclude: ["**/LICENSE"],
 };
