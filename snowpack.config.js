c7212d1ec4812b623025635ea98cdf362a721fbb
Amderson Hernandez <ahernandezdep@gmail.com>
2024-03-05 19:11:44 -0500

    "return"

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,9 +1,10 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    build: "./build/_dist_",
+    public: "/",
+    src: "/_dist_",
   },
   buildOptions: {
-    baseUrl: "https://workshop-2-lazy.vercel.app/",
+    baseUrl: "",
   },
 };
